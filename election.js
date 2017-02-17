f0d7e1792e803469b23db9a46a730a14dc296d6a
wtung008 <wayne.h.tung@gmail.com>
2017-02-17 16:50:07 -0500

    Add <ul id=voters> to index.html and request/response code to elections.js

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-
-  // Imagination!
-
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'json'
+  }).done(function(responseData){
+      console.log(responseData);
+      $.each(responseData.candidates, function(key, value) {
+        $('<li>').html(value.name + ": Number of Votes: " + value.votes).appendTo('#voters');
+        console.log(value.name)
+      });
+  });
 });
